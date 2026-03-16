6d482644cf48a0b902abb97bfacf93a50c2656dd
Tim Andre Jacobsen <tymmyh@gmail.com>
2026-03-16 07:25:40 +0100

    Add indexes to audits on id

--- Claims/Auditing/AuditContext.cs
+++ Claims/Auditing/AuditContext.cs
@@ -9,5 +9,16 @@ namespace Claims.Auditing
         }
         public DbSet<ClaimAudit> ClaimAudits { get; set; }
         public DbSet<CoverAudit> CoverAudits { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ClaimAudit>()
+                .HasIndex(e => e.ClaimId);
+
+            modelBuilder.Entity<CoverAudit>()
+                .HasIndex(e => e.CoverId);
+        }
     }
 }
