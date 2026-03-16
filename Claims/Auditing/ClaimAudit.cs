db3bddf928ffaca297b140c377689c21c449ac7c
Tim Andre Jacobsen <tymmyh@gmail.com>
2026-03-16 07:21:41 +0100

    add max length to strings

--- Claims/Auditing/ClaimAudit.cs
+++ Claims/Auditing/ClaimAudit.cs
@@ -1,13 +1,17 @@
-﻿namespace Claims.Auditing
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Claims.Auditing
 {
     public class ClaimAudit
     {
         public int Id { get; set; }
 
+        [MaxLength(36)]
         public required string ClaimId { get; set; }
 
         public DateTime Created { get; set; }
 
+        [MaxLength(10)]
         public required string HttpRequestType { get; set; }
     }
 }
