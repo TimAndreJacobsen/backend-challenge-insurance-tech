03d7eb5c992bd494e386c1a1a1ebcbcab5f34c11
Tim Andre Jacobsen <tymmyh@gmail.com>
2026-03-16 07:26:30 +0100

    add migraion for nvarcharmax & indexes

---
+++ Claims/Migrations/20260316062514_AddAuditColumnConstraintsAndIndexes.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Claims.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuditColumnConstraintsAndIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "HttpRequestType",
+                table: "CoverAudits",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoverId",
+                table: "CoverAudits",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HttpRequestType",
+                table: "ClaimAudits",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClaimId",
+                table: "ClaimAudits",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CoverAudits_CoverId",
+                table: "CoverAudits",
+                column: "CoverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClaimAudits_ClaimId",
+                table: "ClaimAudits",
+                column: "ClaimId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CoverAudits_CoverId",
+                table: "CoverAudits");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ClaimAudits_ClaimId",
+                table: "ClaimAudits");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HttpRequestType",
+                table: "CoverAudits",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoverId",
+                table: "CoverAudits",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(36)",
+                oldMaxLength: 36);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HttpRequestType",
+                table: "ClaimAudits",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClaimId",
+                table: "ClaimAudits",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(36)",
+                oldMaxLength: 36);
+        }
+    }
+}
